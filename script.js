74ed46dcba628c0e2645c8544ea1163495825a63
brody king <46272894+brodyking@users.noreply.github.com>
2021-10-10 17:30:14 +0000

    Polished and ready to be published as a release

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Basic setup and custimization
-/* CSS file of your choice */ changeCSS('assets/stylesheets/dark.css')
-/* Should the popup load on start?*/ popup("true")
-/* About Header Popup Text*/ aboutheader("Quickblog.js")
-/* About Me Text*/ aboutme("Quckblog.js is a free javascript library that allows you to create a <i>quick blog</i> in minutes!")
-/* Blog Name*/ blogname("Quickblog.js")
+changeCSS('assets/stylesheets/delete.css')
+aboutheader("Quickblog.js")
+aboutme("Quckblog.js is a free javascript library that allows you to create a <i>quick blog</i> in minutes!")
+blogname("Quickblog.js")
+onclick="openTab('about')"
+
+
+
+
+// DELETE ALL OF THIS DOWN VVVV
+createpost("1.0 Offical Release!","This is the first release of Quickblog.js. I hope you all like it. You can check the docs for information on how to install. We have moved all files to a github respitory. Hosting was too much of a hastle.","10/10/2021")
+var docs = "Welcome to the quickblog.js docs! This page is dedicated to setting up your blog!" 
+
+var docs1 = "<h1>Setup</h1>Goto the offical <a href='https://github.com/brodyking/quickblog.js/releases'>GitHub Repo</a> download the newest release"
+
+var docs2 = "<h1>Customization</h1>When you enter the script.js file you will see a already written script. You can delete everything past line 11. Then change the top part of the file to your information."
+
+var docs3 = '<h1>Writing your first blog post.</h1>Go into your script.js file and type <code>createpost("","","")</code> Inside of the first pair of perithesis, type the title of your post, in the second pair of perithesis, type the body, and in the third, type the date of the blog post. Refesh the page and see your blog post come to life! <b>You can only create a maximum of 50 blog posts.</b>'
+
+var docs4 = '<h1>Creating a extra page.</h1>Like writing a post, type <code>createpage("","") Inside the first bracket, type the title, and in the second braket, type the body. Refresh and see the page. <b>You can only make a maximum of one custom page.</b>'
+createpage("docs",docs + docs1 + docs2 + docs3 + docs4)
