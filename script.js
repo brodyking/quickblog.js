d7987dd8925e79867215272234dad8260dcc6c69
brody king <46272894+brodyking@users.noreply.github.com>
2021-10-20 17:37:31 +0000

    Added a blog post that has the same info as the release. Just wanted to create a post for it inside the test site.

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ footer("Made by <a href='https://github.com/brodyking/quickblog.js'>quickblog.js
 
 // DELETE ALL OF THIS DOWN VVVV
 createpost("1.0 Official Release!","This is the first release of Quickblog.js. I hope you all like it. You can check the docs for information on how to install. We have moved all files to a github respitory. Hosting was too much of a hassle.","10/10/2021")
+createpost("1.1 Patch","Added a footer() function that allows you to change the footer on the page.This can be useful to anyone who wants to add social links or any other text!We also changed yotsubablue.css to have white navigation links.I know that a lot of people who used this theme changed this themselves so it is now a default style.We are aware of the 26 post limit and it will be fixed soon!1.2 will have that issue fixed! If you want to look more in-depth in the changes, check the commits.","10/20/2021")
 var docs = "Welcome to the quickblog.js docs! This page is dedicated to setting up your blog!" 
 
 var docs1 = "<h1>Setup</h1>Goto the official <a href='https://github.com/brodyking/quickblog.js/releases'>GitHub Repo</a> download the newest release"
