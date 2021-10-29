c88ad2f3ee555a9d0703ecf6adbff9a9f7e78f7d
brody king <46272894+brodyking@users.noreply.github.com>
2021-10-29 21:24:11 +0000

    alot. read release of 2.0

--- assets/javascript/quickblog.js
+++ assets/javascript/quickblog.js
@@ -1,3 +1,4 @@
+
 // Tab System
 function closepopup() {
   var popup = document.getElementById("popup");
@@ -6,6 +7,7 @@ function closepopup() {
   } else {
     popup.style.display = "none";
   }
+  document.getElementById("shell").innerHTML += "Tab closed<br>"
 }
 
 function openTab(cityName) {
@@ -15,69 +17,92 @@ function openTab(cityName) {
     x[i].style.display = "none";
   }
   document.getElementById(cityName).style.display = "block";
+  document.getElementById("shell").innerHTML += "<i>" + cityName + "</i> was opened<br>"
 }
 
 openTab("timeline");
-
 
 // Functions
 // Tab title
 function tabtitle(text) {
   document.getElementById("tabtitle").innerHTML = text
+  document.getElementById("shell").innerHTML += "set tabtitle to <i>" + text + "</i><br>"
 }
 //Blog Name
 function blogname(text) {
   document.getElementById("blogname").innerHTML = text
   var blogname = text;
+  document.getElementById("shell").innerHTML += "set blogname to <i>" + text + "</i><br>"
+
 }
 // About Header
 function aboutheader(text) {
   document.getElementById("aboutheader").innerHTML = text
-  document.getElementById("aboutheaderlong").innerHTML = text
+  document.getElementById("shell").innerHTML += "set aboutheader to <i>" + text + "</i><br>"
 }
 // About Me
 function aboutme(text) {
   document.getElementById("aboutme").innerHTML = text
-  document.getElementById("aboutmelong").innerHTML = text
+  document.getElementById("shell").innerHTML += "set aboutme to <i>" + text + "</i><br>"
 }
 // Website Url
 function websiteurl(text) {
   var url = text;
+  document.getElementById("shell").innerHTML += "set websiteurl to <i>" + text + "</i><br>"
 }
 // Hide popup
 function popup(text) {
   if (text === "false") {
     document.getElementById("popup").style.display = "none";
+    document.getElementById("shell").innerHTML += "set popup to <i>" + text + "</i><br>"
   }
 }
 // Watermark
 function footer(text) {
   document.getElementById("watermark").innerHTML = text
+  document.getElementById("shell").innerHTML += "set footer to <i>" + text + "</i><br>"
 }
+// Dissallow theme changing
+function themeswapper(text) {
+  if (text == "false") {
+    document.getElementById("zoneSelect").style.display = "none"
+    document.getElementById("shell").innerHTML += "set themeswapper to <i>" + text + "</i><br>"
+  }
+}
+
 // Creating a post
 var postcount = 0;
 // Create post
-function createpost(title, body, date) {
+function createpost(title, body, date, author) {
   if (postcount < 51) {
     postcount += 1;
-    document.getElementById("listing" + postcount).innerHTML = '<p class="item__urls"><a href="#" onclick="openTab(&quot;' + title + '&quot;);"><strong id="strong">' + title + '</strong></a> <span class="item__siteurl" id="url"></span></p><p class="item__meta"><span>' + date + '</span><a class="item__notes" href="#" onclick="alert(&quot;Publish Date: ' + date +' == Title: ' + title + ' == Author: Admin&quot;)">meta</a><a class="item__notes" href="#" onclick="alert(&quot;' + body + '&quot;)">read</a></p></li>';
+    document.getElementById("listing" + postcount).innerHTML += '<p class="item__urls"><a href="#" onclick="openTab(&quot;' + title + '&quot;);"><strong id="strong">' + title + '</strong></a> <span class="item__siteurl" id="url"></span></p><p class="item__meta"><span>' + date + '</span><a class="item__notes" href="#" onclick="alert(&quot;Written by ' + author + '&quot;)">author</a><a class="item__notes" href="#" onclick="alert(&quot;' + body + '&quot;)">read</a></p></li>';
     document.getElementById("listing" + postcount).style.display = "list-item";
-    document.getElementById("post" + postcount).innerHTML = '<main id="' + title + '" class="main city" style="display: none; padding: 5px;"><p class="left">' + body + '<br /><a href="#" onclick="openTab(&quot;timeline&quot;);">return to timeline</a></p></main>'
+    document.getElementById("post" + postcount).innerHTML = '<main id="' + title + '" class="main city" style="display: none; padding: 5px;"><p class="left">' + body + '<br><br><a href="#" onclick="openTab(&quot;timeline&quot;);">return to timeline</a> &middot; Date: ' + date + ' &middot; Author: ' + author + '</p></main>'
+    document.getElementById("shell").innerHTML += "created a new post called <i>" + title + "</i><br>"
   } else {
     prompt("You have reached the post limit!");
+    document.getElementById("shell").innerHTML += "<span style='color=red'>Error! You have reached the max post amount!</span><br>"
   }
+  
 }
 // Create a page
-var pagecount = 1
+var pagecount = 0
 // Create page
 function createpage(title, body) {
-  if (pagecount = 1) {
+    pagecount += 1;
+    if (pagecount == 2) {
+      document.getElementById("pageslink").style.display = "inline";
+    }
     document.getElementById("page" + pagecount).innerHTML = '<p>' + body + '</p>'
-    document.getElementById("page" + pagecount + "link").innerHTML = '[' + title + ']'
+    if (pagecount == 1) {
+          document.getElementById("page" + pagecount + "link").innerHTML = '&nbsp;[' + title + ']&nbsp;'
+    } else {
+      document.getElementById("page" + pagecount + "link").innerHTML = pagecount + '. ' + title + ''
+    }
     document.getElementById("page" + pagecount + "link").style.display = "inline";
-  } else {
-    alert("You have reached the page limit!");
-  }
+    console.log(pagecount)
+    document.getElementById("shell").innerHTML += "created a new page called <i>" + title + "</i><br>"
 }
 
 // CSS Switcher
@@ -91,6 +116,8 @@ function changeCSS(cssFile, cssLinkIndex) {
   newlink.setAttribute("href", cssFile);
 
   document.getElementsByTagName("head").item(cssLinkIndex).replaceChild(newlink, oldlink);
+  document.getElementById("shell").innerHTML += "set changeCSS to <i>" + cssFile + "</i><br>"
+  
 }
 
 function select() {
@@ -124,4 +151,34 @@ function select() {
   } else if (zone.value == "custom") {
     changeCSS('assets/stylesheets/custom.css')
   } 
+  document.getElementById("shell").innerHTML += "set theme to <i>" + zone.value + "</i><br>"
+}
+
+window.addEventListener('keydown', function (e) {
+  if (e.keyCode == 17 && e.target == document.body) {
+    openTab('switchboard');
+  }
+});
+
+
+function generatePost() {
+  var title = prompt("What should the post be called?");
+  var body = prompt("What should the post body be?");
+  var date = prompt("What should the publish date be?");
+  var author = prompt("What should the post author be?");
+  alert("Copy paste the following script to your package.js file to have the post shown.");
+  document.getElementById("alertxt").innerHTML = "<code>createpost('" + title + "','" + body + "','" + date + "','" + author + "');</code>"
+  document.getElementById("shell").innerHTML += "post was generated using switchboard"
+  openTab('alertpage');
+  document.getElementById("alertxt").execCommand('copy');
+}
+
+function generatePage() {
+  var title = prompt("What should the page be called?");
+  var body = prompt("What should the page body be?");
+  alert("Copy paste the following script to your package.js file to have the post shown.");
+  document.getElementById("alertxt").innerHTML = "<code>createpage('" + title + "','" + body + "');</code>"
+  document.getElementById("shell").innerHTML += "page was generated using switchboard"
+  openTab('alertpage');
+  document.getElementById("alertxt").execCommand('copy');
 }
---
+++ package.js
@@ -0,0 +1,19 @@
+changeCSS('assets/stylesheets/dark.css'); // Default CSS File
+
+aboutheader("aboutheader"); // About Header
+
+aboutme("aboutme"); // About me
+
+blogname("blogname"); // Your Blog Name
+
+tabtitle("tabtitle"); // Text in the tab title
+
+footer("footer"); // Footer text
+
+themeswapper("true"); // Allow the user to change themes
+
+popup("true") // Show the popup at the top of the webpage
+
+createpost("title","body","date","author") // Template Post
+
+createpage("title","body") // Template Page
